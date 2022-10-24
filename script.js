1b4c192026cfe6567fb0d8c12ffccad273e7de4e
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-10-24 16:52:39 -0600

    Pseudocode, Bulma html.

---
+++ script.js
@@ -0,0 +1,20 @@
+// //Register for API key. Dashboard with form inputs. Cities add to list of searched
+// //pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, 
+// // How to retrieve geographical coordinates given a city name
+// //Use local storage-localStorage.setItem('textArea);
+// //construct query url to make API call
+// //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// var currentDay= moment(moment()).format('MMM Do YYY h:mm:ss a')
+// console.log(moment())
+
+
+// var cityEl="city";
+// var appId= "key";
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&appid=" + APIKey;
+
+
+// // fetch(queryURL){
+
+
+// // }
