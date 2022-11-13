6fefb50431660db7e50800f75ac93ed1890732a6
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-11-13 06:28:33 -0700

    api added for 3 day and 5 day fetch

--- script.js
+++ script.js
@@ -1,31 +1,107 @@
 // //Register for API key. Dashboard with form inputs. Cities add to list of searched
-// //pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, 
+// //pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs,
 // // How to retrieve geographical coordinates given a city name
 // //Use local storage-localStorage.setItem('textArea);
 // //construct query url to make API call
 // //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+var APIKey = "7df31ef07a9daa898e410e6debc655f9";
 
-const today=moment();
-$("#currentDay").text(today.format('MMMM Do YYYY'));
-console.log(moment())
 
-var searchForACity="city"
-cityEl = document.getElementById("city")
-console.log(
 
-)
-var day
-var temperature
-var wind
-var humidity
+function callLatQuestionMark() {
+  var city = "Clearwater";
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q= " +
+    city +
+    "&appid=" +
+    APIKey;
+  fetch(queryURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+     callLatQuestionMarkTwo(result.coord.lon, result.coord.lat)
+      //console.log(lat, lon)
+      // callLatQuestionMark(lat, lon);
+      console.log(result);
+    });
+}
 
-// var appId= "key";
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&appid=" + APIKey;
+// event listener should invoke callLatQuestionMark function on submit button
+callLatQuestionMark().addEventListener('click', e => {
+  console.log(e)
+})
+ 
 
+function callLatQuestionMarkTwo(lat, lon) {
+  var queryUrlFiveDay = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+  // var queryUrlFiveDay = ""
+  fetch(queryUrlFiveDay)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+        displayFiveDays(result.list)
+      console.log(result);
+    });
+}
+
+function displayFiveDays(arrayOfTimes) {
+
+    for (let i =0; i < arrayOfTimes.length; i++) {
+        const newDate = new Date(arrayOfTimes[i].dt*1000)
+        
+        // gets us the weather at 1400 for the next 5 days
+        if (newDate.getHours() === 14) {
+         const weatherOfDay = arrayOfTimes[i]
+         console.log(weatherOfDay)
+            // In this block you need to dynamically add content onto your page ei document.createElement / append etc
+          const resultList ={
+            temp:"",
+            wind:"",
+            humidity:"",
+
+          };
+
+
+
+
+            
+        }
+        
+        //console.log(arrayOfTimes[i])
+    }
+
+
+}
+ 
+//const today=moment();
+//$("#currentDay").text(today.format('MMMM Do YYYY'));
+//console.log(moment())
+//const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=7df31ef07a9daa898e410e6debc655f9'
+//console.log()
+
+//Variables for location
+//var location = document.getElementById("lon,lat")
+//console.log("lon,lat")
+
+//navigator.geolocation.getCurrentLocation(getLatLon);
+//var latitude = location.coords.latitude;
+//var longitude = location.coords.longitude;
+//function getLatLon(location) {
+//console.log(latitude);
+//}
+// var location ="longitude, latitude"
+// console.log(location)
+// var day
+// var temperature
+// var wind
+// var humidity
+// cityEl.
+// }
 
 // // fetch(queryURL){
-
 
 // // }
 // localStorage.setItem
